a0fed24c3b8903dd5e7922538b02182845d0ac6b
Prajeet Guha <58776463+PrajeetGuha@users.noreply.github.com>
2023-01-28 22:52:40 +0530

    Add files via upload

---
+++ JavaProgrammingAndOOPs/Banking/src/datamodel/SavingsAccount.java
@@ -0,0 +1,10 @@
+package datamodel;
+
+public class SavingsAccount extends Account {
+
+    public SavingsAccount(float amount) {
+        super(amount);
+        this.minimumBalance = 0;
+    }
+
+}
